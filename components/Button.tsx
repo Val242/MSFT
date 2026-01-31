b45a38645234c522af4f2b238a1eff00b0202d8a
=Val242 <=ebongvalentine70@gmail.com>
2026-01-31 19:44:18 +0100

    Hero section advancing

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
 
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: "navigation" | "cta1" | "cta2" | "book"
+  size?: "sm" | "md" | "lg" | "icon";
+};
 
-export default function Button({children}: {children: React.ReactNode}) {
-  return (
-   <button className='bg-[#10217D] text-white border text-sm rounded-2xl w-41 h-11'>
+
+export default function Button({
+  children,
+  variant = "navigation",
+  size = "md",
+  className = "",
+  ...props
+}: ButtonProps & { children: React.ReactNode }) {
+  const varianStyles: Record<string,string> = {
+    navigation:
+    "bg-[#10217D] text-white border text-sm rounded-2xl "
+  }
+
+  const sizeStyles: Record<string,string> = {
+      md:"w-41 h-11 rounded-2xl"
+ }
+  
+   return (
+   <button className={`${varianStyles} ${sizeStyles} ${className} `}>
     {children}
    </button>
-  )
+)
 }
  
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,8 +12,19 @@ export default function Hero() {
         priority
       />
 
-      <div className="relative z-10 text-white">
-        Hello World
+      <div className="relative z-10 bg-white grid-row-[1fr_2fr] ">
+        <div className="flex">
+              
+        </div>
+    <div>
+        <button className="border-black">
+        I need to call for
+        </button>
+        <button className="bg-[#1BA9B5]">
+            Select the Reason
+        </button>
+    </div>
+    
       </div>
     </div>
   )
