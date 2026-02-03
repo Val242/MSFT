8bd71056aa3b8fa2af7c886bb69a2ab7eb6cc265
=Val242 <=ebongvalentine70@gmail.com>
2026-02-03 17:20:19 +0100

    Hero section advancing

--- components/HealthCard.tsx
+++ components/HealthCard.tsx
@@ -11,9 +11,10 @@ interface HealthProp{
 
 function HealthCard({image, text}:HealthProp) {
   return (
-   <div className='flex'>
-        <button className='rounded-[50px] bg-[#1BA9B5] w-20 h-20'> <img src={image.src} height={image.height} width={image.width} alt={image.alt} className='w-10 mx-auto' /></button>
-        <p>{text}</p>
+    
+   <div className='flex bg-[#D8EFF1] w-70 h-30 rounded-[10px] place-content-center justify-center gap-3'>
+        <button className='rounded-[50px] bg-[#1BA9B5] w-15 h-15 my-auto '> <img src={image.src} height={image.height} width={image.width} alt={image.alt} className='w-10 mx-auto' /></button>
+        <p className='my-auto text-[#527C88]'>{text}</p>
    </div>
   )
 }
