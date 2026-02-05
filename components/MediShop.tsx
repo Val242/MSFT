ded1c0e245d83cb40ecfff48ba08dea57785dd59
=Val242 <=ebongvalentine70@gmail.com>
2026-02-05 08:12:23 +0100

    Adjusting MediShop Component

--- components/MediShop.tsx
+++ components/MediShop.tsx
@@ -38,17 +38,12 @@ export default function MediShop() {
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}   >
-         {/* <Image
-        src={bgImage}
-        alt="Background"
-        fill
-        className="object-cover"
-        priority
-      /> */}
+   
       
             <div className="flex items-center justify-center z-10 p-8 ">
-              <h1><span>Shop</span> <span> Medicines & Welness</span></h1>
+        
               <div className="bg-white rounded-[5px] shadow-lg p-8 flex flex-col items-center gap-6 my-8 mx-4">
+                      <h1><span>Shop</span> <span> Medicines & Welness</span></h1>
                    <div className="flex gap-4">
                   {
                     mediWell.map((well,index)=>(
