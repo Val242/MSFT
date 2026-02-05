f962a1e27f84fb9b94295d85c218119f7e7cf9dc
=Val242 <=ebongvalentine70@gmail.com>
2026-02-05 08:37:20 +0100

    Adjusting MediShop Component

--- components/MediShop.tsx
+++ components/MediShop.tsx
@@ -44,7 +44,7 @@ export default function MediShop() {
       
             <div className="flex items-center justify-center z-10 p-8 ">
         
-              <div className="bg-white rounded-[5px] shadow-lg p-8 flex flex-col items-center gap-6 my-8  mx-4">
+              <div className="bg-white rounded-[5px] shadow-lg px-8 py-12 flex flex-col items-center gap-6 my-8  mx-4">
                       <h1 className='font-bold text-3xl'><span className='text-[#1BA9B5]'>Shop</span> <span className='text-[#10217D]'> for Medicines & Welness</span>  <ShoppingCart size={40} color="#10217D" className='inline-block ' /> </h1>
                       <p className='text-center text-[#B7BCD8] text-sm'>Lorem Ipsum is simply dummy text of the printing and typesetting <br /> industry. Lorem Ipsum has been the industry's standard <br /> dumbfbfdbdbdbdbdbd</p>
                    <div className="flex gap-4">
