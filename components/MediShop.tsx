7af399a3d679bf5cc22efba0a92998fcf8477447
=Val242 <=ebongvalentine70@gmail.com>
2026-02-05 09:30:56 +0100

    Optimized the Doctors component

--- components/MediShop.tsx
+++ components/MediShop.tsx
@@ -45,12 +45,23 @@ export default function MediShop() {
             <div className="flex items-center justify-center z-10 p-8 ">
         
               <div className="bg-white rounded-[5px] shadow-lg px-8 py-12 flex flex-col items-center gap-6 my-8  mx-4">
-                      <h1 className='font-bold text-3xl'><span className='text-[#1BA9B5]'>Shop</span> <span className='text-[#10217D]'> for Medicines & Welness</span>  <ShoppingCart size={40} color="#10217D" className='inline-block ' /> </h1>
-                      <p className='text-center text-[#B7BCD8] text-sm'>Lorem Ipsum is simply dummy text of the printing and typesetting <br /> industry. Lorem Ipsum has been the industry's standard <br /> dumbfbfdbdbdbdbdbd</p>
+                      <h1 className='font-bold text-3xl'>
+                        <span className='text-[#1BA9B5]'>Shop</span>
+                        <span className='text-[#10217D]'> for Medicines & Welness</span>  
+                        <ShoppingCart size={40} color="#10217D" className='inline-block ' /> 
+                      </h1>
+                      <p className='text-center text-[#B7BCD8] text-sm'>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting <br /> 
+                        industry. Lorem Ipsum has been the industry's standard <br />
+                        dumbfbfdbdbdbdbdbd
+                        </p>
                    <div className="flex gap-4">
                   {
                     mediWell.map((well,index)=>(
-                        <Wellness image={well.image} name={well.name} key={index}/>
+                        <Wellness
+                          image={well.image}
+                          name={well.name}
+                          key={index}/>
                     ))
                   }
                 </div>
