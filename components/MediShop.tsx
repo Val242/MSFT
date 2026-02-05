99f2e32dc47e1a06ce074c7bb9eaf35916080ef0
=Val242 <=ebongvalentine70@gmail.com>
2026-02-05 08:32:59 +0100

    Adjusting MediShop Component

--- components/MediShop.tsx
+++ components/MediShop.tsx
@@ -7,6 +7,8 @@ import skinCare from "../images/skinCare.png"
 import wellness from "../images/wellness.png"
 import sexualWellness from "../images/sexualWellness.png"
 import Wellness from './Wellness'
+import shoppingCart from '../images/shopping-cart.png'
+import { ShoppingCart } from 'lucide-react';
 
 export default function MediShop() {
     const mediWell = [
@@ -43,7 +45,7 @@ export default function MediShop() {
             <div className="flex items-center justify-center z-10 p-8 ">
         
               <div className="bg-white rounded-[5px] shadow-lg p-8 flex flex-col items-center gap-6 my-8 mx-4">
-                      <h1 className='font-bold text-3xl'><span className='text-[#1BA9B5]'>Shop</span> <span className='text-[#10217D]'> for Medicines & Welness</span></h1>
+                      <h1 className='font-bold text-3xl'><span className='text-[#1BA9B5]'>Shop</span> <span className='text-[#10217D]'> for Medicines & Welness</span>  <ShoppingCart size={40} color="#10217D" className='inline-block ' /> </h1>
                       <p className='text-center text-[#B7BCD8] text-sm'>Lorem Ipsum is simply dummy text of the printing and typesetting <br /> industry. Lorem Ipsum has been the industry's standard <br /> dumbfbfdbdbdbdbdbd</p>
                    <div className="flex gap-4">
                   {
