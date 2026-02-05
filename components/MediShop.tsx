907a2b1f4634b05816fcf8827ec201008214fc89
=Val242 <=ebongvalentine70@gmail.com>
2026-02-05 08:13:53 +0100

    Adjusting MediShop Component

--- components/MediShop.tsx
+++ components/MediShop.tsx
@@ -43,7 +43,7 @@ export default function MediShop() {
             <div className="flex items-center justify-center z-10 p-8 ">
         
               <div className="bg-white rounded-[5px] shadow-lg p-8 flex flex-col items-center gap-6 my-8 mx-4">
-                      <h1><span>Shop</span> <span> Medicines & Welness</span></h1>
+                      <h1 className='font-bold text-3xl'><span>Shop</span> <span> Medicines & Welness</span></h1>
                    <div className="flex gap-4">
                   {
                     mediWell.map((well,index)=>(
