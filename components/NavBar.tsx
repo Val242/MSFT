1a771859f0c0b5e82745aaee8c6cc7fe0c7b8860
=Val242 <=ebongvalentine70@gmail.com>
2026-02-07 13:30:04 +0100

    Almost done

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -11,30 +11,39 @@ export default function NavBar() {
     "Services",
     "About"
   ]
+  
   return (
     <div>
-        <nav className='grid grid-cols-[1fr_3fr_1fr] mt-2 mx-15  '>
-            <Image src={MSFT} alt={'Logo'} className='w-30 h-7 place-items-center'/>
-           
-         <div className='flex gap-2 mx-auto items-center '>
-             
-              {
-                      navBarText.map((text, index)=>(
-                        <div className='hover:text-[#1BA9B5]'  key={index}>
-                          {text}
-                          </div>
-                      ))
-                    }</div>
-         
-            <div className='flex gap-2'>
-              <p className='place-items-center'>Call 671369673 </p>  
-                     
-                <Button variant='navigation' size='md' className=''> Consult Online</Button>
+      <nav className='grid grid-cols-[1fr_3fr_1fr] mt-2 mx-4 md:mx-15 items-center'>
+        
+       
+        <div className='flex justify-start'>
+          <Image 
+            src={MSFT} 
+            alt='Logo' 
+            className='w-24 h-7 md:w-30 md:h-7'
+          />
+        </div>
+        
+        
+        <div className='flex flex-col sm:flex-row gap-2 sm:gap-4 justify-center items-center'>
+          {navBarText.map((text, index) => (
+            <div 
+              className='hover:text-[#1BA9B5] text-center sm:text-left'
+              key={index}
+            >
+              {text}
             </div>
+          ))}
+        </div>
+        
+        
+        <div className='flex flex-col sm:flex-row gap-2 justify-end items-center text-center sm:text-right'>
+          <p className='text-sm'>Call 671369673</p>
+          <Button variant='navigation' size='md'>Consult Online</Button>
+        </div>
 
-        </nav>
-
-
+      </nav>
     </div>
   )
 }
